de12ecff4af4ee949e95bd7f68a17e7ba01273fb
Jason Kraus <jasonk@cukerinteractive.com>
2012-02-17 17:07:02 -0800

    added admin tools menu item for listing custom applications

--- dockitcms/contrib/dockit_admin_tools/menu.py
+++ dockitcms/contrib/dockit_admin_tools/menu.py
@@ -2,7 +2,7 @@ from django.utils.text import capfirst
 
 from admin_tools.menu.items import MenuItem
 
-from dockitcms.models import Collection
+from dockitcms.models import Collection, Application
 
 class CollectionList(MenuItem):
     def __init__(self, title=None, models=None, exclude=None, **kwargs):
@@ -29,3 +29,41 @@ class CollectionList(MenuItem):
 
     def is_empty(self):
         return len(self.children) == 0
+
+class ApplicationList(MenuItem):
+    def __init__(self, title=None, models=None, exclude=None, **kwargs):
+        """
+        ``CollectionList`` constructor.
+        """
+        self.title = title
+        self.models = list(models or [])
+        self.exclude = list(exclude or [])
+        self._init = False
+        super(ApplicationList, self).__init__(title, **kwargs)
+
+    def init_with_context(self, context):
+        if self._init:
+            return
+        items = Application.objects.all()
+        collections = Collection.objects.all()
+        for application in items:
+            #if application.key in self.exclude:
+            #    continue
+            title = capfirst(application.name)
+            #url = collection.get_admin_manage_url()
+            item = MenuItem(title=title)
+            #TODO this is not efficient
+            for collection in collections:
+                if collection.application == application:
+                    item.children.append(self.create_menu_item_for_collection(collection))
+            self.children.append(item)
+        self._init = True
+    
+    def create_menu_item_for_collection(self, collection):
+        title = capfirst(collection.title)
+        url = collection.get_admin_manage_url()
+        return MenuItem(title=title, url=url)
+    
+    def is_empty(self):
+        return len(self.children) == 0
+
