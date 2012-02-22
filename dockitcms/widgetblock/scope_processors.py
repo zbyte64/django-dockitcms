d6eaa157d48f8512c98126b96283dbe646379f95
Jason Kraus <jasonk@cukerinteractive.com>
2012-02-22 15:38:13 -0800

    model associated widgets now functioning

--- dockitcms/widgetblock/scope_processors.py
+++ dockitcms/widgetblock/scope_processors.py
@@ -1,5 +1,6 @@
 from django.db.models import Model
 from django.contrib.contenttypes.models import ContentType
+from django.core.urlresolvers import reverse
 
 from models import ModelWidgets
 
@@ -7,12 +8,10 @@ def widgets(scope):
     if 'object' in scope.kwargs:
         obj = scope.kwargs['object']
         if hasattr(obj, 'widgets'):
-            #TODO compute admin url
             manage_urls = {}
             if hasattr(obj, 'get_manage_urls'):
                 base_url = obj.get_manage_urls()['edit']
                 manage_urls['list'] = base_url + '?_dotpath=widgets'
-            print obj, manage_urls
             scope.add_data('widgets', obj.widgets, manage_urls)
 
 def modelwidgets(scope):
@@ -22,10 +21,15 @@ def modelwidgets(scope):
             ct = ContentType.objects.get_for_model(obj)
             pk = obj.pk
             #TODO make this a lazy lookup
-            entries = ModelWidgets.objects.filter(content_type=ct, object_id=pk)
+            entries = ModelWidgets.objects.filter(content_type=ct, object_id=str(pk))
             widgets = list()
             for entry in entries:
                 widgets.extend(entry.widgets)
             #TODO compute admin url
-            scope.add_data('widgets', widgets)
+            url = reverse('admin:widgetblock_modelwidgets_lookup',
+                          kwargs={'app_label':ct.app_label,
+                                  'module_name':ct.model,
+                                  'pk':pk,})
+            manage_urls = {'list':url}
+            scope.add_data('widgets', widgets, manage_urls)
 
