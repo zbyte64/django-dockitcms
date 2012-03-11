7da8c0b1b57834d24ad29e36a8d83833a9ab0491
Jason Kraus <jasonk@cukerinteractive.com>
2012-03-10 18:22:54 -0800

    quick fix for demo

--- dockitcms/middleware.py
+++ dockitcms/middleware.py
@@ -45,9 +45,11 @@ class DockitCMSMiddleware(object):
 
 class DefaultScopeMiddleware(object):
     def process_template_response(self, request, response):
-        context = response.context_data
-        if 'scopes' not in context:
-            context['scopes'] = [get_site_scope()]
-            if 'object' in context and not any([scope.name == 'object' for scope in context['scopes']]):
-                context['scopes'].append(Scope(name='object', object=context['object']))
+        if hasattr(response, 'context_data'): #TODO this shouldn't happen
+            context = response.context_data
+            if 'scopes' not in context:
+                context['scopes'] = [get_site_scope()]
+                if 'object' in context and not any([scope.name == 'object' for scope in context['scopes']]):
+                    context['scopes'].append(Scope(name='object', object=context['object']))
         return response
+
