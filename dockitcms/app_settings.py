499a40e2e16f4955a6fcd1700268c9c7b70873f2
Jason Kraus <zbyte64@gmail.com>
2012-06-18 20:13:38 -0700

    quick fix/hack

--- dockitcms/app_settings.py
+++ dockitcms/app_settings.py
@@ -4,13 +4,22 @@ try:
 except ImportError:
     from django.utils import importlib
 
-SCOPE_PROCESSORS = list()
+class LazyList(list):
+    _loaded = False
+    
+    def __iter__(self):
+        if not self._loaded:
+            self.load()
+    
+    def load(self):
+        for entry in getattr(settings, 'SCOPE_PROCESSORS', []):
+            module_name, class_name = entry.rsplit('.', 1)
+            module = importlib.import_module(module_name)
+            obj = getattr(module, class_name)
+            if isinstance(obj, type):
+                obj = obj()
+            self.append(obj)
+        self._loaded = True
 
-for entry in getattr(settings, 'SCOPE_PROCESSORS', []):
-    module_name, class_name = entry.rsplit('.', 1)
-    module = importlib.import_module(module_name)
-    obj = getattr(module, class_name)
-    if isinstance(obj, type):
-        obj = obj()
-    SCOPE_PROCESSORS.append(obj)
+SCOPE_PROCESSORS = LazyList()
 
