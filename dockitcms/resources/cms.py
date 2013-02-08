af49f999093218d4a7d0d7528da41d0fb0acde6e
Jason Kraus <zbyte64@gmail.com>
2013-02-07 17:25:56 -0800

    provide app name when registering resources

--- dockitcms/resources/cms.py
+++ dockitcms/resources/cms.py
@@ -11,15 +11,17 @@ from dockitcms.resources.common import ReloadCMSSiteMixin, CMSDocumentResource
 
 #from common import CMSDocumentResource, AdminAwareSchemaAdmin
 
+app_name = 'dockitcms'
+
 class ApplicationResource(CMSDocumentResource):
     pass
 
-hyperadmin.site.register(Application, ApplicationResource)
+hyperadmin.site.register(Application, ApplicationResource, app_name=app_name)
 
 class DocumentDesignResource(CMSDocumentResource):
     pass
 
-hyperadmin.site.register(DocumentDesign, DocumentDesignResource)
+hyperadmin.site.register(DocumentDesign, DocumentDesignResource, app_name=app_name)
 
 class CollectionResource(ReloadCMSSiteMixin, CMSDocumentResource):
     list_display = ['title', 'application']#, 'admin_manage_link']
@@ -38,25 +40,25 @@ class CollectionResource(ReloadCMSSiteMixin, CMSDocumentResource):
         klass = item.get_resource_class()
         return klass(**kwargs)
 
-hyperadmin.site.register(Collection, CollectionResource)
+hyperadmin.site.register(Collection, CollectionResource, app_name=app_name)
 
 class IndexResource(ReloadCMSSiteMixin, CMSDocumentResource):
     pass
 
-hyperadmin.site.register(Index, IndexResource)
+hyperadmin.site.register(Index, IndexResource, app_name=app_name)
 
 class SubsiteResource(ReloadCMSSiteMixin, CMSDocumentResource):
     list_display = ['name', 'url']
 
-hyperadmin.site.register(Subsite, SubsiteResource)
+hyperadmin.site.register(Subsite, SubsiteResource, app_name=app_name)
 
 class SubsiteResourceDefinitionResource(ReloadCMSSiteMixin, CMSDocumentResource):
     list_display = ['name', 'subsite', 'collection', 'url']
 
-hyperadmin.site.register(SubsiteResourceDefinition, SubsiteResourceDefinitionResource)
+hyperadmin.site.register(SubsiteResourceDefinition, SubsiteResourceDefinitionResource, app_name=app_name)
 
 class RecipeResource(CMSDocumentResource):
     pass
 
-hyperadmin.site.register(BaseRecipe, RecipeResource)
+hyperadmin.site.register(BaseRecipe, RecipeResource, app_name=app_name)
 
